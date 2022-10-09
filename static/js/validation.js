dfa686ec025eb98e47a6f3188839fe7694ff077d
Yeferson.Flores <floresy@uninorte.edu.co>
2022-10-08 23:14:00 -0500

    feat: validation routine was added and tested under rendering

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,3 +1,30 @@
+function validar_formulario() {
+    var username = document.formRegistro.usuario;
+    var email = document.formRegistro.correo;
+    var passid = document.formRegistro.pass;
+  
+    var username_len = username.value.length;
+    if (username_len == 0 || username_len < 8) {
+        alert("Debes ingresar un username con min. 8 caracteres");
+        username.focus();
+        return false; //Para la parte dos, que los datos se conserven
+    }
+
+    var formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+    if (!email.value.match(formato_email)) {
+        alert("Debes ingresar un email electronico valido!");
+        email.focus();
+        return false; //Para la parte dos, que los datos se conserven
+    }
+
+    var passid_len = passid.value.length;
+    if (passid_len == 0 || passid_len < 8) {
+        alert("Debes ingresar una password con mas de 8 caracteres");
+        passid.focus();
+        return false;
+    }
+ }
+
 function showPassword(){
     let pass1 = document.getElementById("pass");
     if(pass1.type == "password")
@@ -5,6 +32,7 @@ function showPassword(){
         pass1.type = "text";
     }
 }
+
 function hidePassword(){
     document.getElementById("pass").type = "password";
 }
